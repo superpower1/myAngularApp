fbbb9557c39e6cfb56c2389cb45cc489ee40d752
superpower1 <superpiglet1@gmail.com>
2017-08-28 23:23:47 +1000

    add my-app angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+	id: number;
+	name: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Vrains';
+  hero: Hero = {
+		id: 1,
+		name: 'Playmaker'
+	};
 }
