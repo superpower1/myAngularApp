ef3931d6f095d660ec61cdb40fb14305dc95e92b
superpower1 <superpiglet1@gmail.com>
2017-08-31 18:06:06 +1000

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-	id: number;
-	name: string;
-}
-
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
   title = 'Vrains';
-  hero: Hero = {
-		id: 1,
-		name: 'Playmaker'
-	};
 }
